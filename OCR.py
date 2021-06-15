f8d356acf19a4720b045b21e80f79f90c4943447
Jorge Donis <jdda2@alu.ua.es>
2021-06-15 10:35:14 +0200

    vamos para la biblio

--- OCR.py
+++ OCR.py
@@ -14,9 +14,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-import os
-
-
 import matplotlib.pyplot as plt
 from typing import List, Tuple
 import numpy as np
@@ -27,13 +24,14 @@ import tensorflow.keras as K
 import cv2
 import glob
 import re
-import os
+from os import system
 
+NUM_IMAGES_DATASET = 384151
 BATCH_SIZE = 4
 IMG_HEIGHT = 32
 CHAR_LIST = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 BLANK_CHARACTER = len(CHAR_LIST)
-POOLING_RATIO = 16
+POOLING_RATIO = 8
 
 def encode_str(txt : str) -> List[int]:
     # encoding each output word into digits
@@ -43,7 +41,10 @@ def encode_str(txt : str) -> List[int]:
     return dig_lst
 
 def load_image_label(image_path : str) -> Tuple[np.ndarray, str]:
-    img = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2GRAY) 
+    img = cv2.imread(image_path)
+    if (img is None):
+        return None, None
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
     img = np.expand_dims(img, axis=2)
     img = pad_img_vertical(img, IMG_HEIGHT)
     img = img / 255
@@ -73,6 +74,8 @@ def train_generator() -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     labels = []
     for image_path in glob.iglob("./OCR_dataset_2/*.jpg"):
         img, label = load_image_label(image_path)
+        if (img is None):
+            continue
         images.append(img)
         labels.append(label)
         i += 1
@@ -114,21 +117,17 @@ def ctc_lambda_func(args):
 
 def get_activation_training_models() -> Tuple[object, object]:
     inputs = tf.keras.layers.Input(shape=(IMG_HEIGHT, None, 1), name='padded_images')
+
     conv_1 = tf.keras.layers.Conv2D(64, (3,3), activation = 'relu', padding='same')(inputs)
     pool_1 = tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=2)(conv_1)
     conv_2 = tf.keras.layers.Conv2D(128, (3,3), activation = 'relu', padding='same')(pool_1)
     pool_2 = tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=2)(conv_2)
     conv_3 = tf.keras.layers.Conv2D(256, (3,3), activation = 'relu', padding='same')(pool_2)
-    conv_4 = tf.keras.layers.Conv2D(256, (3,3), activation = 'relu', padding='same')(conv_3)
-    pool_4 = tf.keras.layers.MaxPool2D(pool_size=(2, 1))(conv_4)
-    conv_5 = tf.keras.layers.Conv2D(512, (3,3), activation = 'relu', padding='same')(pool_4)
-    batch_norm_5 = tf.keras.layers.BatchNormalization()(conv_5)
-    conv_6 = tf.keras.layers.Conv2D(512, (3,3), activation = 'relu', padding='same')(batch_norm_5)
-    batch_norm_6 = tf.keras.layers.BatchNormalization()(conv_6)
-    pool_6 = tf.keras.layers.MaxPool2D(pool_size=(2, 2))(batch_norm_6)
-    conv_7 = tf.keras.layers.Conv2D(512, (2,2), activation = 'relu')(pool_6)
-    squeezed = tf.keras.layers.Lambda(lambda x: tf.squeeze(x, 1))(conv_7)
-    blstm_1 = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(units = 128, input_shape=[None], return_sequences=True, dropout = 0.2))(squeezed)
+    batch_norm = tf.keras.layers.BatchNormalization()(conv_3)
+    pool_3 = tf.keras.layers.MaxPool2D(pool_size=(2, 2))(batch_norm)
+    conv_4 = tf.keras.layers.Conv2D(512, (2,2), activation = 'relu', padding='same')(pool_3)
+    reshape = tf.keras.layers.Lambda(lambda x: tf.reshape(x, [tf.shape(conv_4)[0], -1, 512 * 4])) (conv_4)
+    blstm_1 = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(units = 128, input_shape=[None], return_sequences=True, dropout = 0.2))(reshape)
     blstm_2 = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(units = 128, input_shape=[None], return_sequences=True, dropout = 0.2))(blstm_1)
     outputs = tf.keras.layers.Dense(len(CHAR_LIST) + 1, activation = 'softmax')(blstm_2)
     act_model = tf.keras.Model(inputs, outputs)
@@ -139,7 +138,7 @@ def get_activation_training_models() -> Tuple[object, object]:
 
     loss_out = tf.keras.layers.Lambda(ctc_lambda_func, output_shape=(1,), name='ctc')([outputs, labels, input_length, label_length])
     training_model = tf.keras.Model(inputs=[inputs, labels, input_length, label_length], outputs=loss_out)
-    training_model.compile(loss = {'ctc' : lambda y_true, y_pred: y_pred}, optimizer = 'adam')
+    training_model.compile(loss = { 'ctc' : lambda y_true, y_pred: y_pred }, optimizer = 'adam')
 
     return training_model, act_model
 
@@ -148,8 +147,24 @@ training_model, act_model = get_activation_training_models()
 filepath="best_model.hdf5"
 checkpoint = K.callbacks.ModelCheckpoint(filepath=filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='auto')
 callbacks_list = [checkpoint]
-act_model.summary()
+# act_model.summary()
 training_model.summary()
+
+num_bad_images = 0
+for i in range(0, 384151 // BATCH_SIZE):
+    # print(F"i = {i}")
+    inputs_fit, outputs_train, shape = next(generator)
+    for j in range(0, BATCH_SIZE):
+        if ((inputs_fit['padded_images'][j].shape[1] // POOLING_RATIO) < inputs_fit['original_label_lengths'][j]):
+            # img = inputs_fit['padded_images'][j]
+            # show_img(img)
+            # print(F"Padded_width = {img.shape[1]}")
+            # print(F"Original_width = {inputs_fit['original_image_lengths'][j]}")
+            # print(F"Original label length = {inputs_fit['original_label_lengths'][j]}")
+            num_bad_images = num_bad_images + 1
+        
+print(F"Number of bad images : {num_bad_images} ({num_bad_images / 384151 * 100}%)")
+
 # training_model.fit(x = generator, callbacks = callbacks_list)
 
 
