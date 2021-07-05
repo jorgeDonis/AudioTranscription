846b056dcce909fc3f19f29f1ce0c38a8e0bd377
Jorge Donis <jdda2@alu.ua.es>
2021-07-05 12:48:01 +0200

    before preprocessing all images in dataset

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -17,6 +17,8 @@
 import cv2
 import numpy as np
 
+import tensorflow as tf
+
 def preprocess_img(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
     img = np.expand_dims(img, axis=2)
@@ -27,3 +29,6 @@ def preprocess_img(img):
 #Pads with black pixels (0's)
 def pad_img_horizontal(img, max_img_len):
     return np.pad(img, ( (0, 0), (0, max_img_len - img.shape[1]), (0, 0) ), 'constant', constant_values= ( (0, 0), (0, 0), (0, 0) ))
+
+def preprocess_all_dataset(img):
+    return
